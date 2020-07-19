f75fdc3902a7a8f40fecc272779a928a1eada185
Adam Eilers <adam.eilers@gmail.com>
2020-07-19 17:26:11 -0500

    adding HTTP/2 support and wired up routers

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 export const NODE_ENV = process.env.NODE_ENV ?? 'production'
 export const PORT = process.env.PORT ?? 3000
+export const SSL_CERT_PATH = process.env.SSL_CERT_PATH ?? '/cert/ssl-cert.pem'
+export const SSL_KEY_PATH = process.env.SSL_KEY_PATH ?? '/cert/ssl-key.pem'
